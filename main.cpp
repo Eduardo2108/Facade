17b0150b2fc1a533ba2b3e8598cd05237d4e6dc1
eduardo218 <eduardo212792@gmail.com>
2021-03-25 10:51:28 -0600

    Finished facade pattern

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-int main() {
+#include "src/Fachada/Facade.h"
 
+int main() {
+    Facade *api = new Facade();
+    //podemos ejecutar métodos en diferentes objetos, mediante un solo método, en vez de realizar las llamadas independientes
+    //si los parametros cambian, solo tendrémos que modificar las llamadas dentro del facade y no en todo el código.
+
+    api->execute1();
+    api->execute2();
     return 0;
 }
